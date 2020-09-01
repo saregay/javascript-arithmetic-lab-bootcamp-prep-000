b0cc94522a4a5082d732412342fea0a79fe8e827
semira <edyorase@gmail.com>
2020-09-01 22:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var number = 5
 
-number++ // 5
+function add2 () {
+  number +=2
+}
 
+function subtract4 () {
+  number -=1
+}
+
+function multiply5 () {
+  number *=5
+}
+
+function divide5 (){
+  number 
+}
